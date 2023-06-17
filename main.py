118530bba2793247fbe5e0a64157c6813b8b63cd
alisadaintanvir <alisadaintanvir@gmail.com>
2023-06-17 08:12:48 +0600

    Add High Score feature

--- main.py
+++ main.py
@@ -30,15 +30,17 @@ while game_is_on:
     if snake.head.distance(food) < 15:
         food.refresh()
         snake.extend()
-        scoreboard.refresh()
+        scoreboard.increase_score()
 
     if snake.head.xcor() > 295 or snake.head.xcor() < -295 or snake.head.ycor() > 295 or snake.head.ycor() < -295:
-        game_is_on = False
-        scoreboard.game_over()
+        scoreboard.reset()
+        snake.reset()
 
     for segment in snake.segments[1:]:
-        if snake.head.distance(segment) < 10:
-            game_is_on = False
-            scoreboard.game_over()
+        if segment == snake.head:
+            pass
+        elif snake.head.distance(segment) < 10:
+            scoreboard.reset()
+            snake.reset()
 
 screen.exitonclick()
